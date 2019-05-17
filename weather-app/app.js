bbe529461ec859de1dd840155c1141236851d7cc
Ricardo Balarini <ricardovbalarini@gmail.com>
2019-05-17 10:52:54 -0300

    bringing my first prediction

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,9 +2,11 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/37b57ad6ea80743cfb46c79fc995c548/37.8267,-122.4233'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({url: url, json: true}, (error, response) => {
+    const data = response.body.currently
+    const degrees = data.temperature
+    const chance = data.precipProbability
+    console.log('It is currently '+ degrees +' out. There is a '+ chance +'% chance of rain.')
 })
 
 SECRET_KEY = '37b57ad6ea80743cfb46c79fc995c548'
