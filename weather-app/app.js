1fba743e65beb9f5d7e5f30038d4fe074e357817
Ricardo Balarini <ricardovbalarini@gmail.com>
2019-05-17 11:04:58 -0300

    with celsius and lang set

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,17 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/37b57ad6ea80743cfb46c79fc995c548/37.8267,-122.4233'
+const SECRET_KEY = '37b57ad6ea80743cfb46c79fc995c548'
+
+// const api = 'https://api.darksky.net/forecast/key/latitude,longitude'
+const url = 'https://api.darksky.net/forecast/37b57ad6ea80743cfb46c79fc995c548/37.8267,-122.4233?lang=pt&units=si'
 
 request({url: url, json: true}, (error, response) => {
     const data = response.body.currently
     const degrees = data.temperature
     const chance = data.precipProbability
-    console.log('It is currently '+ degrees +' out. There is a '+ chance +'% chance of rain.')
+    const daily = response.body.daily
+    const summary = daily.data[0].summary
+    console.log(summary + ' It is currently '+ degrees +' out. There is a '+ chance +'% chance of rain.')
 })
 
-SECRET_KEY = '37b57ad6ea80743cfb46c79fc995c548'
+
