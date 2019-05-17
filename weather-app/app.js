9a61c949521ee34217871956e76d2aa83e33072a
Ricardo Balarini <ricardovbalarini@gmail.com>
2019-05-17 10:30:01 -0300

    starting weather-app

---
+++ weather-app/app.js
@@ -0,0 +1,13 @@
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('2 Second Timer')
+}, 2000)
+
+setTimeout(() => {
+    console.log('0 Second Time')
+}, 0)
+
+console.log('Stoping')
+
+SECRET_KEY = '37b57ad6ea80743cfb46c79fc995c548'
